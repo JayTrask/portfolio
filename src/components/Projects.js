026b4e61c1cab707db31f4dbf3f1ab96740fc0b2
JayTrask <jontrask@umass.edu>
2019-05-13 22:22:53 -0400

    Changed color scheme on images and text for projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ThreeScene from './ThreeScene.js';
 import dubs from '../imgs/dubs.png'
 import leetcode from '../imgs/leetcode.png'
+import temp from '../imgs/temp.png'
 
 
 class Projects extends Component{
@@ -12,7 +13,7 @@ class Projects extends Component{
 				<div className="row row-projects">
 
 					<div className="card project">
-						<img src="https://images.pexels.com/photos/2210285/pexels-photo-2210285.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" className="img-project"></img>
+						<img src={temp} className="img-project"></img>
 						<div className="container">
 							<h4><b>Hivelist</b></h4>
 							<p>Collaboratively build playlists with a unique voting system.</p>
@@ -36,7 +37,6 @@ class Projects extends Component{
 					</div>
 
 				</div>
-
 
 			
 
