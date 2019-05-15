edc6837bfc76f0469a39a29466544b7d13c66c82
JayTrask <jontrask@umass.edu>
2019-05-14 22:30:07 -0400

    Added temporary links for each of the projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,27 +13,27 @@ class Projects extends Component{
 				<div className="row row-projects">
 
 					<div className="card project">
-						<img src={temp} className="img-project"></img>
+						<a href="https://github.com/JayTrask/HiveList"><img src={temp} className="img-project"></img>
 						<div className="container">
 							<h4><b>Hivelist</b></h4>
 							<p>Collaboratively build playlists with a unique voting system.</p>
-						</div>
+						</div></a>
 					</div>
 
 					<div className="card project">
-						<img src={dubs} className="img-project"></img>
+						<a href="https://www.dubsonline.com/"><img src={dubs} className="img-project"></img>
 						<div className="container">
 							<h4><b>Dub's Online</b></h4>
 							<p>Static website built for hometown liquor store.</p>
-						</div>
+						</div></a>
 					</div>
 
 					<div className="card project">
-						<img src={leetcode} className="img-project"></img>
+						<a href="https://github.com/JayTrask/challenges"><img src={leetcode} className="img-project"></img>
 						<div className="container">
 							<h4><b>Coding Challenges</b></h4>
 							<p>Solutions to Leetcode, Hackerrank, and similar challenges.</p>
-						</div>
+						</div></a>
 					</div>
 
 				</div>
